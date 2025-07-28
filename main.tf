057844256773d78aac10a05beddb0d0eff599a1b
tejaswiniforaws <tejaswiniforaws@gmail.com>
2025-07-28 12:21:51 +0530

    terraform practice

--- main.tf
+++ main.tf
@@ -3,6 +3,7 @@ module "vpc" {
   for_each = var.vpc
   vpc_cidr = lookup(each.value,"vpc_cidr",null)
   subnets_cidr = lookup(each.value,"subnets_cidr",null)
+  az = lookup(each.value,"az",null)
 
   env      = var.env
   project_name = var.project_name
